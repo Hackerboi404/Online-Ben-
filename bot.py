2325a10713f8c9030c6a037cd9cef8bf1f703d97
Hackerboi404 <tuturahul14rk@gmail.com>
2026-05-02 19:07:22 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = "8345687203:AAEloDRtx3ymHyuKDXbkUfjpnKvisTTbrMQ"
+WEB_URL = "https://ben-tennyson.onrender.com"
+
+async def stream(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("🎬 Open Stream", url=WEB_URL)]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Click below to watch stream 👇",
+        reply_markup=reply_markup
+    )
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("stream", stream))
+
+app.run_polling()
